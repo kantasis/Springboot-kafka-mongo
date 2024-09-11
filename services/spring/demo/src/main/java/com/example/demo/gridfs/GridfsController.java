3652fbe5ed24b89ee4dd8cf378d591016a4783f3
George <kantasisg@gmail.com>
2024-09-11 14:31:14 +0300

    Updated swagger for the files

--- services/spring/demo/src/main/java/com/example/demo/gridfs/GridfsController.java
+++ services/spring/demo/src/main/java/com/example/demo/gridfs/GridfsController.java
@@ -10,12 +10,20 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import java.util.ArrayList;
+import lombok.extern.log4j.Log4j2;
 
 @RestController
+@Log4j2
 @CrossOrigin("*")
 @RequestMapping("api/")
 public class GridfsController {
@@ -23,22 +31,70 @@ public class GridfsController {
    @Autowired
    private GridfsService gridfsService;
 
-   @PostMapping("upload")
-   public ResponseEntity<?> upload(
+   @PostMapping("files")
+   @Operation(
+      summary = "Upload a file",
+      description = "Store a multipart file to the data lake. Returns the ID of the file stored."
+   )
+   @ApiResponses({
+      @ApiResponse(
+         responseCode = "200",
+         description = "Successful response",
+         content = { 
+            @Content(
+               schema = @Schema(implementation = ResponseEntity.class), 
+               mediaType = "application/json"
+            )
+         }
+      ),
+   })
+   public ResponseEntity<String> upload(
       @RequestParam("file")
       MultipartFile file
    ) throws IOException {
       String storedID_str = gridfsService.saveFile(file);
-      return new ResponseEntity<>(storedID_str, HttpStatus.OK);
+      return new ResponseEntity<String>(storedID_str, HttpStatus.OK);
    }
 
-   @GetMapping("download/{id}")
+   @GetMapping("files/{id}")
+   @Operation(
+      summary = "Download a file",
+      description = "Gets the file that matches the ID param."
+   )
+   @ApiResponses({
+      @ApiResponse(
+         responseCode = "200",
+         description = "Successful response",
+         content = { 
+            @Content(
+               schema = @Schema(implementation = ResponseEntity.class), 
+               mediaType = "application/json"
+            )
+         }
+      ),
+      @ApiResponse(
+         responseCode = "404",
+         description = "ID not found in the data lake",
+         content = { 
+            @Content(
+               schema = @Schema(implementation = ResponseEntity.class), 
+               mediaType = "application/json"
+            )
+         }
+      ),
+   })
    public ResponseEntity<ByteArrayResource> download(
       @PathVariable 
       String id
    ) throws IOException {
 
       GridfsModel gridfsModel = gridfsService.loadFile(id);
+
+      if (gridfsModel == null)
+         return ResponseEntity
+            .status(HttpStatus.NOT_FOUND)
+            // .notFound()
+            .body(null);
 
       return ResponseEntity
          .ok()
@@ -51,7 +107,33 @@ public class GridfsController {
          .body(new ByteArrayResource(gridfsModel.getBytes()));
    }
 
-   @GetMapping("list")
+   @GetMapping("files")
+   @Operation(
+      summary = "List the files in the data lake",
+      description = "Gets the file that matches the ID param."
+   )
+   @ApiResponses({
+      @ApiResponse(
+         responseCode = "200",
+         description = "Successful response",
+         content = { 
+            @Content(
+               schema = @Schema(implementation = ResponseEntity.class), 
+               mediaType = "application/json"
+            )
+         }
+      ),
+      @ApiResponse(
+         responseCode = "404",
+         description = "ID not found in the data lake",
+         content = { 
+            @Content(
+               schema = @Schema(implementation = ResponseEntity.class), 
+               mediaType = "application/json"
+            )
+         }
+      ),
+   })
    public ResponseEntity<Map<String, String>> list() throws IOException {
       
       Map<String, String> files_hashMap = gridfsService.listAllFiles();
@@ -61,7 +143,33 @@ public class GridfsController {
          .body(files_hashMap);
    }
 
-   @DeleteMapping("delete/{id}")
+   @DeleteMapping("files/{id}")
+   @Operation(
+      summary = "Delete a file",
+      description = "Delete the file with the specified ID."
+   )
+   @ApiResponses({
+      @ApiResponse(
+         responseCode = "200",
+         description = "Successful response",
+         content = { 
+            @Content(
+               schema = @Schema(implementation = ResponseEntity.class), 
+               mediaType = "application/json"
+            )
+         }
+      ),
+      @ApiResponse(
+         responseCode = "204",
+         description = "ID not found in the data lake",
+         content = { 
+            @Content(
+               schema = @Schema(implementation = ResponseEntity.class), 
+               mediaType = "application/json"
+            )
+         }
+      ),
+   })
    public ResponseEntity<String> delete(
       @PathVariable 
       String id
